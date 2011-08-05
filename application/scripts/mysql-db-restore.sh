b5ba037c1aa7661149a5af04ed236bc12a61b36f
Roni <roni@citrusinformatics.com>
2011-08-05 10:15:06 +0530

    first commit

---
+++ application/scripts/mysql-db-restore.sh
@@ -0,0 +1,9 @@
+#!/bin/sh 
+#This will restore MySql database
+mysql_password=
+mysql_dbname=$1
+application_mysql_dump_location_for_dbrestore=$2
+mysql --user=root --password=$mysql_password  $application_mysql_dbname < $application_mysql_dump_location_for_dbrestore
+echo "Database restored"
+
+
